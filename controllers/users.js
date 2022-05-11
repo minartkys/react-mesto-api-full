ffcd4908fb18cf981efecb958cc450296f383c4c
Artyom Minyaev <minart19992@gmail.com>
2022-05-11 14:45:18 +0300

    First rewie 15

---
+++ controllers/users.js
@@ -0,0 +1,145 @@
+/* eslint-disable no-shadow */
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
+const AuthorizationError = require('../errors/UnauthorizedError');
+const NotFoundError = require('../errors/NotFoundError');
+const BadRequestError = require('../errors/BadRequestError');
+const ConflictingRequestError = require('../errors/ConflictingRequestError');
+
+module.exports.getUserById = (req, res, next) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (user === null) {
+        throw new NotFoundError('Пользователь по указанному _id не найден.');
+      }
+      res.status(200).send({
+        data: user,
+      });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Пользователь по указанному _id не найден.'));
+      }
+      next(err);
+    });
+};
+
+module.exports.getUsers = (req, res, next) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(next);
+};
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
+
+    .then((user) => User.findById(user._id))
+    .then((user) => {
+      res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(
+          new BadRequestError(
+            'Введите имя, информацию о себе, ссылку на аватар, почту и пароль',
+          ),
+        );
+      }
+      if (err.code === 11000) {
+        return next(
+          new ConflictingRequestError('Такой пользователь уже существует'),
+        );
+      }
+      return next(new Error('Произошла ошибка'));
+    });
+};
+
+module.exports.updateUser = (req, res, next) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (user === null) {
+        throw new NotFoundError('Пользователь с указанным _id не найден.');
+      }
+      res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(
+          new BadRequestError(
+            'Переданы некорректные данные при обновлении профиля.',
+          ),
+        );
+      }
+      next(err);
+    });
+};
+
+module.exports.updateAvatar = (req, res, next) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (!user) {
+        return next(
+          new NotFoundError('Пользователь по указанному _id не найден.'),
+        );
+      }
+      return res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(
+          new BadRequestError(
+            'Переданы некорректные данные при обновлении пользователя',
+          ),
+        );
+      }
+      return next(err);
+    });
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', {
+        expiresIn: '7d',
+      });
+      res.send({ token });
+    })
+    .catch(() => next(new AuthorizationError('Неправильные почта или пароль.')));
+};
+
+module.exports.getUserMe = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => {
+      if (user) {
+        res.status(200).send(user);
+      }
+      return next(new NotFoundError('Пользователь с указанным _id не найден.'));
+    })
+    .catch(next);
+};
---
+++ routes/users.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const {
+  getUsers,
+  getUserById,
+  updateUser,
+  updateAvatar,
+  getUserMe,
+} = require('../controllers/users');
+
+router.get('/users', getUsers);
+router.get(
+  '/users/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().length(24).hex(),
+    }),
+  }),
+  getUserById,
+);
+router.get('/users/me', getUserMe);
+
+router.patch(
+  '/users/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  updateUser,
+);
+router.patch(
+  '/users/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string()
+        .required()
+        .pattern(
+          /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/,
+        ),
+    }),
+  }),
+  updateAvatar,
+);
+module.exports = router;
